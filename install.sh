e4f9b3fcc2ff093e4bbf77b5a1969809a6375906
VGBrandon <villegas.galvan.brandon@gmail.com>
2024-08-22 16:25:42 -0500

    delete nwg-look and replace with lxappearance

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ sudo pacman -Syu --noconfirm
 
 # Instala aplicaciones necesarias
 echo -e "${BLUE}Instalando aplicaciones oficiales...${NC}"
-sudo pacman -S --noconfirm neovim git neofetch waybar thunar font-manager tree curl zsh zsh-completions os-prober ntfs-3g tumbler ffmpegthumbnailer thunar-archive-plugin thunar-media-tags-plugin thunar-volman ark gvfs polkit-gnome stow hyprpaper gnome-keyring seahorse nodejs npm nwg-look
+sudo pacman -S --noconfirm neovim git neofetch waybar thunar font-manager tree curl zsh zsh-completions os-prober ntfs-3g tumbler ffmpegthumbnailer thunar-archive-plugin thunar-media-tags-plugin thunar-volman ark gvfs polkit-gnome stow hyprpaper gnome-keyring seahorse nodejs npm lxappearance
 
 # Configurar Git
 echo -e "${CYAN}Configurando Git...${NC}"
@@ -47,16 +47,16 @@ yay -S --noconfirm thunar-shares-plugin google-chrome visual-studio-code-bin
 echo -e "${BLUE}Cambiando la shell predeterminada a Zsh...${NC}"
 chsh -s $(which zsh)
 
-echo -e "${BLUE}Instalando gtk de gruvbox-material...${NC}"
-git clone https://aur.archlinux.org/gruvbox-material-theme-git.git
-cd gruvbox-material-theme-git
-sudo -u $USER makepkg -si --noconfirm
-cd ..
-sudo rm -rf gruvbox-material-theme-git
 
 echo -e "${BLUE}Creando symlinks de mis dotfiles...${NC}"
 chmod +x rename-exist-files-for-execute-stow.sh
 ./rename-exist-files-for-execute-stow.sh
+
+echo -e "${BLUE}Instalando gtk de gruvbox-material...${NC}"
+git clone https://github.com/TheGreatMcPain/gruvbox-material-gtk.git
+cp -r gruvbox-material-gtk/themes/* ~/.themes
+cp -r gruvbox-material-gtk/icons/* ~/.icons
+sudo rm -rf gruvbox-material-gtk
 
 echo -e "${BLUE}Agregando configuracion adicional de git para las llaves...${NC}"
 git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
