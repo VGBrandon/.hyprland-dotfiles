6a87b4e1c1828e68898adb3441da74240ab5377e
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-01-15 01:47:29 -0500

    update verification shell

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,2 +1,4 @@
-# Cambiar shell predetermina a Zsh
-chsh -s $(which zsh)
+#!/bin/bash
+
+# Cambiar a zsh solo si no estás usando zsh
+[[ "$SHELL" != *"zsh"* ]] && chsh -s $(which zsh) && echo "Shell cambiado a zsh. Cierra y abre de nuevo tu sesión."
