5c7d14cb1604e168494c24a3d5b08cdbcbf25949
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-01-17 08:41:49 -0500

    use script for title ascii

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-echo "Iniciando instalación..."
+source $HOME/.hyprland-dotfiles/scripts/tools/title-to-ascii.sh
+
+title_to_ascii "Starting Installation" "green"
 
 # Paso 1: Instalar paquetes oficiales
-./packages/official.sh
+title_to_ascii "Install official packages" "blue"
+$HOME/.hyprland-dotfiles/scripts/install/packages/official.sh
 
 # Paso 2: Instalar paquetes AUR
-./packages/aur.sh
+title_to_ascii "Install AUR packages" "blue"
+$HOME/.hyprland-dotfiles/scripts/install/packages/aur.sh
 
 # Paso 3: Configurar dotfiles con stow
-../stow/stow-all.sh
+title_to_ascii "Configure dotfiles with Stow" "blue"
+$HOME/.hyprland-dotfiles/scripts/stow/stow-all.sh
 
 # Paso 4: Configuración post-instalación
-./post-install.sh
+title_to_ascii "Configure post install" "blue"
+$HOME/.hyprland-dotfiles/scripts/install/post-install.sh
 
+title_to_ascii "Installation Complete" "green"
 echo "¡Instalación completada!"
