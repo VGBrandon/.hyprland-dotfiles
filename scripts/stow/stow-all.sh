b0bb3910366a9f09058db6093088150d06aca0dc
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-01-17 01:07:43 -0500

    update

--- scripts/stow/stow-all.sh
+++ scripts/stow/stow-all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Cambiar al directorio raíz de .hyprland-dotfiles
-cd "$(cd "$(dirname "$0")"/.. && pwd)" || exit
-
 # Verificar conflictos antes de aplicar los dotfiles
-#./stow/stow-check.sh
+../tools/prepare-stow.sh
+
+# Cambiar al directorio raíz de .hyprland-dotfiles
+cd $HOME/.hyprland-dotfiles
 
 # Aplicar todos los dotfiles usando stow para carpetas
 echo "Aplicando carpetas de dotfiles..."
