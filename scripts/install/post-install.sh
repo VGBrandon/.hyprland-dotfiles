399e7f9095877ccd67a9c5ae0023dfd2d5ffff65
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-01-17 08:54:30 -0500

    execute script for configure hyprland

--- scripts/install/post-install.sh
+++ scripts/install/post-install.sh
@@ -2,3 +2,6 @@
 
 # Cambiar a zsh solo si no estás usando zsh
 [[ "$SHELL" != *"zsh"* ]] && chsh -s $(which zsh) && echo "Shell cambiado a zsh. Cierra y abre de nuevo tu sesión."
+
+# Reemplazar contenido de hyprland.conf
+$HOME/.hyprland-dotfiles/scripts/tools/configure-hyprland.sh
