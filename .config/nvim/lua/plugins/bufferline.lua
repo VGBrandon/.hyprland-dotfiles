0a7d25a1beb2aac0f137a8ffa7389ce08a8d81a0
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-01-18 22:34:14 -0500

    update

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -7,9 +7,9 @@ return {
 	config = function()
 		require("bufferline").setup({
 			options = {
-				mode = "buffers", -- set to "tabs" to only show tabpages instead
-				themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
-				numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+				mode = "buffers",          -- set to "tabs" to only show tabpages instead
+				themable = true,           -- allows highlight groups to be overriden i.e. sets highlights as default
+				numbers = "none",          -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
 				close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
 				buffer_close_icon = "✗",
 				close_icon = "✗",
@@ -22,8 +22,8 @@ return {
 				tab_size = 21,
 				diagnostics = false,
 				diagnostics_update_in_insert = false,
-				color_icons = true,
-				show_buffer_icons = true,
+				color_icons = false,
+				show_buffer_icons = false,
 				show_buffer_close_icons = true,
 				show_close_icon = true,
 				persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
