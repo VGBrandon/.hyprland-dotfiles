fd983b738c723a46c394937e3dfc6a9278e3ea0b
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-01-20 00:20:38 -0500

    update padding

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,20 +3,20 @@ local wezterm = require("wezterm")
 config = wezterm.config_builder()
 
 config = {
-  automatically_reload_config = true,
-  enable_tab_bar = false,
-  window_close_confirmation = "NeverPrompt",
-  --window_decorations = "RESIZE",
-  default_cursor_style = "SteadyBlock",
-  color_scheme = "Ashes (base16)",
-  font = wezterm.font("JetBrains Mono", { weight = "DemiBold" }),
-  font_size = 10.6,
-  window_padding = {
-    left = 20,
-    right = 20,
-    top = 20,
-    bottom = 0,
-  },
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	--window_decorations = "RESIZE",
+	default_cursor_style = "SteadyBlock",
+	color_scheme = "Ashes (base16)",
+	font = wezterm.font("JetBrains Mono", { weight = "DemiBold" }),
+	font_size = 10.6,
+	window_padding = {
+		left = 5,
+		right = 5,
+		top = 5,
+		bottom = 5,
+	},
 }
 
 return config
