5f0b38ee495962a83c4615751bcfb133234ebf32
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-02-09 17:31:33 -0500

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,13 +8,13 @@ config = {
 	window_close_confirmation = "NeverPrompt",
 	--window_decorations = "RESIZE",
 	default_cursor_style = "SteadyBlock",
-	color_scheme = "Ashes (base16)",
+	color_scheme = "Catppuccin Mocha (Gogh)",
 	font = wezterm.font("JetBrains Mono", { weight = "DemiBold" }),
 	font_size = 10.6,
 	window_padding = {
-		left = 10,
-		right = 10,
-		top = 10,
+		left = 0,
+		right = 0,
+		top = 0,
 		bottom = 0,
 	},
 }
